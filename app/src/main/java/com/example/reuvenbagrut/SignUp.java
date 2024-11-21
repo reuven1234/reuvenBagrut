6bfc7f913a938775c057ac5a6278bf8d941b46d8
reuven1234 <guchner123@gmail.com>
2024-11-21 11:39:07 +0200

    Sign Up

--- app/src/main/java/com/example/reuvenbagrut/SignUp.java
+++ app/src/main/java/com/example/reuvenbagrut/SignUp.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -17,18 +20,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SignUp extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputEditText;
 
+public class SignUp extends AppCompatActivity {
+    TextView GoBack;
+    Button SignUp;
+    EditText editTextName,editTextPassword,editTextEmail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
-
-        TextView GoBack = findViewById(R.id.GoBack);
         SpannableString spannableString = new SpannableString("Already have an account? Login");
-        Button SignUp = findViewById(R.id.SignUp);
+        editTextName= findViewById(R.id.Name);
+        editTextPassword= findViewById(R.id.Password);
+        editTextEmail= findViewById(R.id.Email);
+        GoBack = findViewById(R.id.GoBack);
+        SignUp = findViewById(R.id.SignUp);
 
         ClickableSpan clickableSpan = new ClickableSpan() {
             @Override
@@ -43,12 +52,25 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SignUp.this,Home.class);
                 startActivity(intent);
+
+                String email,password,name;
+                email = editTextEmail.getText().toString();
+                password = editTextPassword.getText().toString();
+                name = editTextName.getText().toString();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    Toast.makeText(com.example.reuvenbagrut.SignUp.this,"Enter email",Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         spannableString.setSpan(clickableSpan, 25, 30, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         GoBack.setText(spannableString);
         GoBack.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+
 
     }
 }

348ca403d1c844d0038836e02279f422cde907c3
reuven1234 <guchner123@gmail.com>
2024-11-24 13:31:33 +0200

    SignUp and Login working

--- app/src/main/java/com/example/reuvenbagrut/Home.java
+++ app/src/main/java/com/example/reuvenbagrut/Home.java
@@ -1,6 +1,10 @@
 package com.example.reuvenbagrut;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,14 +12,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
 public class Home extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    Button logout;
+    TextView logoutTxt;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+        logout = findViewById(R.id.LogOut);
+        logoutTxt = findViewById(R.id.LogOutTxt);
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
 
+        if(user == null)
+        {
+            Intent intent = new Intent(Home.this,Login.class);
+            startActivity(intent);
+            finish();
+        }
+        else
+        {
+            logoutTxt.setText(user.getEmail());
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(Home.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/reuvenbagrut/SignUp.java
+++ app/src/main/java/com/example/reuvenbagrut/SignUp.java
@@ -20,24 +20,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
     TextView GoBack;
     Button SignUp;
     EditText editTextName,editTextPassword,editTextEmail;
+    FirebaseAuth mAuth;
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(SignUp.this,Home.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
-
+        mAuth = FirebaseAuth.getInstance();
         SpannableString spannableString = new SpannableString("Already have an account? Login");
         editTextName= findViewById(R.id.Name);
         editTextPassword= findViewById(R.id.Password);
         editTextEmail= findViewById(R.id.Email);
         GoBack = findViewById(R.id.GoBack);
         SignUp = findViewById(R.id.SignUp);
+
 
         ClickableSpan clickableSpan = new ClickableSpan() {
             @Override
@@ -52,6 +69,7 @@ public class SignUp extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(SignUp.this,Home.class);
                 startActivity(intent);
+                finish();
 
                 String email,password,name;
                 email = editTextEmail.getText().toString();
@@ -75,6 +93,21 @@ public class SignUp extends AppCompatActivity {
                     Toast.makeText(com.example.reuvenbagrut.SignUp.this,"Enter name",Toast.LENGTH_SHORT).show();
                     return;
                 }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(SignUp.this, "Account created.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(SignUp.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
 

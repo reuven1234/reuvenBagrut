05113fbb77a1a440c95ffa228c4c2d7aa1836bf1
reuven4123 <reuven4123@gmail.com>
2025-01-05 01:16:53 +0200

    settings button work

--- app/src/main/java/com/example/reuvenbagrut/Profile_nav_Fragment.java
+++ app/src/main/java/com/example/reuvenbagrut/Profile_nav_Fragment.java
@@ -9,11 +9,17 @@ import android.view.LayoutInflater;
 import android.view.SurfaceControl;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.reuvenbagrut.databinding.FragmentProfileNavBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Profile_nav_Fragment extends Fragment {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    TextView hiTxt;
 
     public Profile_nav_Fragment() {
         // Required empty public constructor
@@ -23,18 +29,25 @@ public class Profile_nav_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_settings_nav_, container, false);
-
-        settings.findViewById(R.id.settings);
+        View view = inflater.inflate(R.layout.fragment_profile_nav_, container, false);
+        hiTxt = view.findViewById(R.id.helloTxt);
+        settings = view.findViewById(R.id.settings);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Profile_nav_Fragment fragment1 = new Profile_nav_Fragment();
+                Settings_nav_Fragment fragment1 = new Settings_nav_Fragment();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.flFragment,fragment1);
                 transaction.commit();
             }
         });
+
+        if(user != null)
+        {
+            String name = "";
+            name = user.getDisplayName();
+            hiTxt.setText("Hello " + name);
+        }
 
         return view;
     }
